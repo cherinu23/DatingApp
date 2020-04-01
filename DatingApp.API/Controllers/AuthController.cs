b72aeb6cd5d46054ebb811f97014767cf6d43e9e
cherinu23 <rotaru.alexandru23@yahoo.com>
2020-04-01 15:24:17 +0300

    updates

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -54,7 +54,6 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForRegisterDto userForLoginDto)
         {
-           // throw new Exception("Computer says no!");
 
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
